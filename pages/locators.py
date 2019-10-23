fd96ca3fc1abd61241f2e74f1a0edca6e9014088
opanasenko <vla.opanasenko@gmail.com>
2019-10-23 23:37:30 +0300

    added new test and configured page object

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,7 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    PRODUCT_PAGE = (By.XPATH, '//h1[text()="The shellcoder\'s handbook"]')
+    PRODUCT_PRICE = (By.CLASS_NAME, '[class="price_color"]')
---
+++ pages/product_page.py
@@ -0,0 +1,5 @@
+from .base_page import BasePage
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        pass
