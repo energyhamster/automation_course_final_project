8e39381638622db11682036c53a879dde4c23002
opanasenko <vla.opanasenko@gmail.com>
2019-10-24 00:51:00 +0300

    updated new test

--- pages/locators.py
+++ pages/locators.py
@@ -9,5 +9,8 @@ class LoginPageLocators():
     REGISTRATION_FORM = (By.ID, "register_form")
 
 class ProductPageLocators():
-    PRODUCT_PAGE = (By.XPATH, '//h1[text()="The shellcoder\'s handbook"]')
-    PRODUCT_PRICE = (By.CLASS_NAME, '[class="price_color"]')
+    PRODUCT_NAME_ON_PRODUCT_PAGE = (By.XPATH, '//h1[text()="The shellcoder\'s handbook"]')
+    PRODUCT_PRICE_ON_PRODUCT_PAGE = (By.CLASS_NAME, "price_color")
+    PRODUCT_PAGE_SUBMIT_BUTTON = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_NAME_IN_BASKET = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_PRICE_IN_BASKET = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
