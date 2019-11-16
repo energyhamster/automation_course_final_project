2d01a111dd26bb154986e02f43a3920b6c123331
opanasenko <vla.opanasenko@gmail.com>
2019-11-16 12:38:02 +0200

    improve tests and fraimwork

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,7 @@ class ProductPageLocators():
     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
     PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, "#messages > div.alert:nth-child(3) strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "//div[@id='messages']//div[1]/div//strong//following-sibling::text()")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
