5914506fe91b414d8f1b0e342470be9f3ff16750
opanasenko <vla.opanasenko@gmail.com>
2019-12-03 00:47:15 +0200

    changed tests

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,3 +4,11 @@ class BasketPage(BasePage):
     def should_be_basket_url(self):
         # проверка на корректный url адрес
         assert "/basket/" in self.browser.current_url, "Incorrect basket URL"
+
+    def basket_is_empty(self):
+        # checking that basket is empty
+        pass
+
+    def basket_is_empty_text(self):
+        # checking that basket is empty text presented
+        pass
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -24,11 +24,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_basket_url()
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_basket_url()
